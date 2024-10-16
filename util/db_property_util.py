f081ff79f57fb50a700a0ab46181219c747eb6cd
Nikhilsai16802 <nikhilsai16802@gmail.com>
2024-10-16 09:58:48 +0530

    Add files via upload

---
+++ util/db_property_util.py
@@ -0,0 +1,4 @@
+class DBPropertyUtil:
+    @staticmethod
+    def get_connection_string():
+        return 'Driver={SQL Server};Server=NIKKYPC\SQLEXPRESS;Database=TechShopDB;Trusted_Connection=yes;'
